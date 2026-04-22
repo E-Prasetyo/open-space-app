706923d3aff0e88b8b4febb51b071d1c763d1dd6
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2026-04-23 00:04:08 +0700

    make test pass

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
   test: {
     environment: 'jsdom',
   },
