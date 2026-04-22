28da1be895dda9431076a05c2b0718413da00be9
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2026-04-22 23:44:55 +0700

    make test pass

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput(defaultValue = '') {
     setValue(target.value);
   }
 
-  return [value, handleValue, setValue];
+  return [value, handleValueChange, setValue];
 }
 
 export default useInput;
